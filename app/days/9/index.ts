696a42924557aa9c14a1428b4a6cfd0b960b3151
Peder Fosse <peder.fosse@gmail.com>
2023-12-09 19:16:56 +0100

    day9part1

--- app/days/9/index.ts
+++ app/days/9/index.ts
@@ -1,11 +1,34 @@
 import type { Task } from '../task';
 
 export class Day9 implements Task {
-  implemented = false;
   solve(input: string): Record<string, unknown> {
+    const lines = input.split('\n');
+
+    const sum = lines
+      .map((val) => {
+        const values = val.split(' ').map(Number);
+        return getNextNumber(values);
+      })
+      .reduce((prev, cur) => prev + cur, 0);
+
     return {
-      implemented: false,
+      sum,
     };
   }
 }
 
+function getNextNumber(sequence: number[]): number {
+  if (sequence.every((val) => val === 0)) {
+    return 0;
+  }
+
+  const nextSequence: number[] = [];
+  sequence.forEach((item, index) => {
+    if (index !== 0) {
+      const x = item - sequence[index - 1];
+      nextSequence.push(x);
+    }
+  });
+
+  return sequence[sequence.length - 1] + getNextNumber(nextSequence);
+}
