21a6faf9459e7c08cd96daa852cadfb29afd641f
Peder Fosse <peder.fosse@gmail.com>
2023-12-14 07:40:56 +0100

    day13part1

--- app/days/11/index.ts
+++ app/days/11/index.ts
@@ -8,4 +8,3 @@ export class Day11 implements Task {
     };
   }
 }
-
--- app/days/13/index.ts
+++ app/days/13/index.ts
@@ -1,11 +1,69 @@
 import type { Task } from '../task';
 
 export class Day13 implements Task {
-  implemented = false;
   solve(input: string): Record<string, unknown> {
+    let sum = 0;
+
+    input
+      .trim()
+      .split('\n\n')
+      .forEach((pattern, i) => {
+        const rows = pattern.split('\n');
+
+        let horisontalMirrors: number[] | undefined;
+        rows.forEach((row) => {
+          const indices = mirrorIndices(row);
+          if (!horisontalMirrors) {
+            horisontalMirrors = indices;
+          } else {
+            horisontalMirrors = horisontalMirrors.filter((val) => indices.includes(val));
+          }
+        });
+
+        // Find vertical Mirrors
+        let verticalMirrors: number[] | undefined;
+        for (let colIdx = 0; colIdx < rows[0].length; colIdx++) {
+          let currentRow = '';
+          for (let rowIdx = 0; rowIdx < rows.length; rowIdx++) {
+            currentRow += rows[rowIdx][colIdx];
+          }
+          const indices = mirrorIndices(currentRow);
+          if (!verticalMirrors) {
+            verticalMirrors = indices;
+          } else {
+            verticalMirrors = verticalMirrors.filter((val) => indices.includes(val));
+          }
+        }
+
+        if (horisontalMirrors && horisontalMirrors.length === 1) {
+          sum += horisontalMirrors[0];
+        } else {
+          sum += verticalMirrors![0] * 100;
+        }
+      });
+
     return {
-      implemented: false,
+      task1: sum,
     };
   }
 }
 
+function mirrorIndices(row: string): number[] {
+  const indices: number[] = [];
+  for (let colIdx = 1; colIdx < row.length; colIdx++) {
+    let left = colIdx - 1;
+    let right = colIdx;
+    let isMirror = true;
+    while (left >= 0 && right < row.length) {
+      if (row[left] !== row[right]) {
+        isMirror = false;
+      }
+      left--;
+      right++;
+    }
+    if (isMirror) {
+      indices.push(colIdx);
+    }
+  }
+  return indices;
+}
