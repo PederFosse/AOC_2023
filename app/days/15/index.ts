d2ee5384e370d5ba85a980c10955b90154255d1c
Peder Fosse <peder.fosse@gmail.com>
2023-12-26 09:59:54 +0100

    day15part1

--- app/days/15/index.ts
+++ app/days/15/index.ts
@@ -1,11 +1,26 @@
 import type { Task } from '../task';
 
 export class Day15 implements Task {
-  implemented = false;
   solve(input: string): Record<string, unknown> {
+    const sum = input
+      .replaceAll('\n', '')
+      .split(',')
+      .map(hash)
+      .reduce((prev, cur) => prev + cur, 0);
     return {
-      implemented: false,
+      sum,
     };
   }
 }
 
+function hash(input: string) {
+  let currentValue = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    currentValue += input.charCodeAt(i);
+    currentValue *= 17;
+    currentValue %= 256;
+  }
+
+  return currentValue;
+}
